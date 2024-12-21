cee538d07c03b1016cb3e687df08caffbd3b1162
Alison <alison@andersonholland.com>
2024-12-21 21:16:01 +0000

    Assistant checkpoint: Fix missing closing parenthesis in book.js
    Assistant generated file changes:
    - static/book.js: Fix missing closing parenthesis
    
    ---
    
    User prompt:
    
    Unexpected token ')'

--- static/book.js
+++ static/book.js
@@ -12,14 +12,13 @@ $(document).ready(function() {
   });
 });
 
-
 $.get("/api/", function(data){
-  console.log(data)
-})
+  console.log(data);
+});
 
 function displayBooks(data) {
   $(".content").empty();
   for (let i in data){
-    $(".content").append('<div class="card"><h2 class="card-name">' + data[i].title + '</h2><img src="' + data[i].url + '" class="card-image"/><div class="card-body"><p class="card-description">' + '<h3>' + data[i].author + '</h3>' + '<h4>Find it on the shelf</h4><p class="card-details">' + data[i].call_number + '</p><p class="card-date-added">Date added: ' + data[i].date_added + '</p></div></div>')
+    $(".content").append('<div class="card"><h2 class="card-name">' + data[i].title + '</h2><img src="' + data[i].url + '" class="card-image"/><div class="card-body"><p class="card-description">' + '<h3>' + data[i].author + '</h3>' + '<h4>Find it on the shelf</h4><p class="card-details">' + data[i].call_number + '</p><p class="card-date-added">Date added: ' + data[i].date_added + '</p></div></div>');
   }
-})
+}
