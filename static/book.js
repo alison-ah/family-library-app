ee3cec8d95396c745051b447c023c434e83284a8
Alison <alison@andersonholland.com>
2024-12-22 21:36:32 +0000

    Assistant checkpoint: Format date to be more readable
    Assistant generated file changes:
    - static/book.js: Format date_added
    
    ---
    
    User prompt:
    
    format date_added

--- static/book.js
+++ static/book.js
@@ -18,7 +18,13 @@ $.get("/api/", function(data){
 function displayBooks(data) {
   $(".content").empty();
   for (let i in data){
-    $(".content").append('<div class="card"><h2 class="card-name">' + data[i].title + '</h2><div class="card-actions"><a href="/edit/' + data[i].title + '" class="edit-btn"><i class="fas fa-edit"></i></a><button onclick="deleteCard(\'' + data[i].title + '\')" class="delete-btn"><i class="fas fa-trash"></i></button></div><img src="' + data[i].url + '" class="card-image"/><div class="card-body"><p class="card-description">' + '<h3>' + data[i].author + '</h3>' + '<h4>Find it on the shelf</h4><p class="card-details">' + data[i].call_number + '</p><p class="card-date-added">Date added: ' + data[i].date_added + '</p></div></div>');
+    const date = new Date(data[i].date_added);
+    const formattedDate = date.toLocaleDateString('en-US', { 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric' 
+    });
+    $(".content").append('<div class="card"><h2 class="card-name">' + data[i].title + '</h2><div class="card-actions"><a href="/edit/' + data[i].title + '" class="edit-btn"><i class="fas fa-edit"></i></a><button onclick="deleteCard(\'' + data[i].title + '\')" class="delete-btn"><i class="fas fa-trash"></i></button></div><img src="' + data[i].url + '" class="card-image"/><div class="card-body"><p class="card-description">' + '<h3>' + data[i].author + '</h3>' + '<h4>Find it on the shelf</h4><p class="card-details">' + data[i].call_number + '</p><p class="card-date-added">Date added: ' + formattedDate + '</p></div></div>');
   }
 }
 function deleteCard(title) {
